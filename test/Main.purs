21045f22499bdf1ede4b60f1c4bacc544b3f5dee
Asad Saeeduddin <masaeedu@gmail.com>
2019-06-22 14:43:54 -0400

    Check that it's an isomorphism

--- test/Main.purs
+++ test/Main.purs
@@ -2,7 +2,7 @@ module Test.Main where
 
 import Prelude
 
-import Data.Lens (view, set)
+import Data.Lens (Iso, re, set, view)
 import Data.Lens.Remap (remap)
 import Data.Symbol (SProxy(..))
 import Effect (Effect)
@@ -11,21 +11,26 @@ import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter (consoleReporter)
 import Test.Spec.Runner (run)
 
+l = remap { "foo": SProxy :: _ "bar" }
+
 main :: Effect Unit
 main = run [consoleReporter] do
 
   describe "purescript-remap" do
-    describe "happy path" do
+    describe "remap" do
       let
-        l = remap { "foo": SProxy :: _ "bar" }
         s = { foo: 42, baz: "quux" }
-        b = { bar: "potato", baz: "quux" }
+        b = { bar: "potato", baz: "quarkle" }
 
       it "should view correctly" do
         (view l s) `shouldEqual` { bar: 42, baz: "quux" }
 
       it "should update correctly" do
-        (set l b s) `shouldEqual` { foo: "potato", baz: "quux" }
+        (set l b s) `shouldEqual` { foo: "potato", baz: "quarkle" }
+
+      it "should cancel out its reverse" do
+        (view (l <<< re l) s) `shouldEqual` s
+        (view (re l <<< l) b) `shouldEqual` b
 
     describe "laws" do
       -- TODO: Fill these in
