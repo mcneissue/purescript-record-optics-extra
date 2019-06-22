4a2ac1607d4d369bb18573f75f0e73a5b10ecc89
Asad Saeeduddin <masaeedu@gmail.com>
2019-06-22 16:17:29 -0400

    Remove incorrect fundeps

--- src/Data/Lens/Remap.purs
+++ src/Data/Lens/Remap.purs
@@ -10,7 +10,7 @@ import Type.Data.RowList (RLProxy(..))
 import Type.Prelude (SProxy(..), class RowToList)
 import Type.Row (class Cons, class Lacks) as R
 
-class Remap (rl :: RowList) (s :: # Type) (t :: # Type) (a :: # Type) (b :: # Type) | rl s -> a, rl a -> s, rl b -> t, rl t -> b, rl a -> b, rl b -> a, rl s -> t, rl t -> s
+class Remap (rl :: RowList) (s :: # Type) (t :: # Type) (a :: # Type) (b :: # Type) | rl s -> a, rl a -> s, rl b -> t, rl t -> b
   where
   remapRL :: forall proxy. proxy rl -> Iso { | s } { | t } { | a } { | b }
 
